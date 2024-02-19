bec45b5654e1879e7c5aa97d2ef9fbe0d7a78d5d
Harrison Njuguna <noqsnoqs@gmail.com>
2024-02-19 15:01:59 +0300

    Add validations to the models

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,2 +1,9 @@
 class Doctor < ApplicationRecord
+    has_many :reservations, dependent: :destroy
+    has_many :users, through: :reservations
+  
+    validates :name, presence: true
+    validates :image, presence: true
+    validates :doctor-type, presence: true
+    validates :city, presence: true
 end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,2 +1,11 @@
 class Reservation < ApplicationRecord
+    belongs_to :user
+    belongs_to :doctor
+  
+    validates :date_of_reservation, presence: true
+    validates :city, presence: true
+  
+    def doctor_name
+      doctor&.name
+    end
 end
