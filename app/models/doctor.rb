1f07d4dcd873d713bd9769455c832d1fee6a23f8
Harrison Njuguna <noqsnoqs@gmail.com>
2024-02-20 19:02:29 +0300

    Fix linters

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,9 +1,9 @@
 class Doctor < ApplicationRecord
-    has_many :reservations, dependent: :destroy
-    has_many :users, through: :reservations
-  
-    validates :name, presence: true
-    validates :image_url, presence: true
-    validates :doctor-type, presence: true
-    validates :city, presence: true
+  has_many :reservations, dependent: :destroy
+  has_many :users, through: :reservations
+
+  validates :name, presence: true
+  validates :image_url, presence: true
+  validates :doctor - type, presence: true
+  validates :city, presence: true
 end
